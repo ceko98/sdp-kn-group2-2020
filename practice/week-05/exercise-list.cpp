8713762db6c06feb5af876ac9a001d47ffe75322
ceko98 <ceko9898@gmail.com>
2020-11-11 22:24:21 +0200

    Add exercise solution problems

--- practice/week-05/exercise-list.cpp
+++ practice/week-05/exercise-list.cpp
@@ -2,12 +2,13 @@
 
 template<class T>
 class List {
-private:
+public:
   struct Node {
     T data;
     Node *next;
   };
 
+private:
   Node *first;
 
 public:
@@ -17,6 +18,12 @@ public:
   void insetAtStart(T data);
   void print();
   void at(T data, int position);
+  void sort();
+  Node* getFirst();
+  List<T>& unionList(List<T>& list);
+  void difference(List<T>& list);
+  void setCyclic();
+  bool detectCycle();
 };
 
 template<class T>
@@ -76,12 +83,162 @@ void List<T>::at(T data, int position) {
   curr->next = newNode;
 }
 
+template<class T>
+void List<T>::sort() {
+  Node *newFirst = nullptr;
+  Node *tmpFirst = this->first;
+  Node *newFirstCurr = nullptr;
+
+  while (tmpFirst != nullptr) {
+    Node *curr = tmpFirst;
+    Node *prev = nullptr;
+
+    Node *currMin = curr, *prevMin = prev;
+    while (curr != nullptr) {
+      if (currMin->data > curr->data) {
+        currMin = curr;
+        prevMin = prev;
+      }
+      prev = curr;
+      curr = curr->next;
+    }
+
+    if (newFirst == nullptr) {
+      newFirst = currMin;
+      newFirstCurr = newFirst;
+    } else {
+      newFirstCurr->next = currMin;
+      newFirstCurr = newFirstCurr->next;
+    }
+    
+    if (currMin == tmpFirst) {
+      tmpFirst = tmpFirst->next;
+    } else {
+      prevMin->next = prevMin->next->next;
+    }
+  }
+
+  this->first = newFirst;
+}
+
+template<class T>
+typename List<T>::Node * List<T>::getFirst() {
+  return this->first;
+} 
+
+template<class T>
+List<T>& List<T>::unionList(List<T>& list) {
+  List<T>::Node *secondFirst = list.getFirst();
+}
+
+template<class T>
+void List<T>::difference(List<T>& list) {
+  Node *secondCurr = list.getFirst();;
+
+  while (secondCurr != nullptr) {
+    Node *firstCurr = this->first;
+    Node *firstPrev = nullptr;
+
+    while (firstCurr != nullptr) {
+      if (secondCurr->data == firstCurr->data) {
+        if (firstCurr == this->first) {
+          this->first = this->first->next;
+        } else {
+          firstPrev->next = firstCurr->next;
+        }
+
+        delete firstCurr;
+        firstCurr = firstPrev->next;
+      }
+
+      firstPrev = firstCurr;
+      firstCurr = firstCurr->next;
+    }
+
+    secondCurr = secondCurr->next;
+  }
+}
+
+template<class T>
+void List<T>::setCyclic() {
+  Node *a1 = new Node();
+  Node *a2 = new Node();
+  Node *a3 = new Node();
+  Node *a4 = new Node();
+
+  a1->next = a2;
+  a2->next = a3;
+  a3->next = a4;
+  a4->next = a2;
+
+  a1->data = 4;
+  a2->data = 1;
+  a3->data = 7;
+  a4->data = 10;
+
+  this->first = a1;
+}
+
+template<class T>
+bool List<T>::detectCycle() {
+  Node *curr1 = this->first;
+  Node *curr2 = this->first;
+
+  while (curr2 != nullptr)
+  {
+    curr1 = curr1->next;
+    curr2 = curr2->next;
+    if (curr2->next) {
+      curr2 = curr2->next;
+    }
+    //           v
+    // 1 -> 2 -> 3 -> nullptr -> segmetation fault
+    if (curr1 == curr2) {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main() {
   List<int> list;
+  List<int> list2;
 
-  list.insetAtStart(3);
-  list.insetAtStart(2);
+  list.insetAtStart(7);
+  list.insetAtStart(4);
   list.insetAtStart(1);
-  
+  list.insetAtStart(10);
+  list.print();
+  std::cout << std::endl;
+  // list.sort();
+  // list.print();
+  // std::cout << std::endl;
+
+  list2.insetAtStart(1);
+  list2.insetAtStart(5);
+
+  list.difference(list2);
+  list.print();
+  std::cout << std::endl;
+
+  List<int> cyclic;
+  cyclic.setCyclic();
+  // cyclic.print();
+  std::cout << cyclic.detectCycle() << std::endl;
+
+  q.front() // position (1,1)
+  int numberOfPositions = 3;
+  while (/**/)
+    q.push(/*new position*/);
+
+
   return 0;
 }
+
+/*
+          v1      v2
+  |10| -> |7| -> |1| -> |4| -|
+            ^----         <---
+
+  newFirst
+*/
