2d6065ab4c63930cdbd6c5a7ab619bdab1ac263a
ceko98 <ceko9898@gmail.com>
2021-01-14 14:11:50 +0200

    Add code from exercise week 13

--- practice/week-13/graph.cpp
+++ practice/week-13/graph.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <set>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -13,27 +14,26 @@ class Graph
 private:
     int numberOfVertecies;
     vector<int> *graph;
-    vector<int> *cost;
+    vector<int> *weights;
     int *prev;
     bool *used;
 
 public:
-    Graph();
+    Graph(bool weighted);
     ~Graph();
-    void init();
+    void init(bool weighted);
     void clear();
     void clearUsed();
     void dfsPrint(int start);
     void bfsPrint(int start);
-    int shortestBetween(int start, int end);
-    void printLevels(int start);
-    int longestBetween(int start, int end);
-    bool isConnected();
+    void findPathDfs(int start, int end);
+    void findShortestPathBfs(int start, int end);
+    void dijkstra(int start, int end);
 };
 
-Graph::Graph()
+Graph::Graph(bool weighted = false)
 {
-    init();
+    init(weighted);
 }
 
 Graph::~Graph()
@@ -41,11 +41,11 @@ Graph::~Graph()
     clear();
 }
 
-void Graph::init()
+void Graph::init(bool weighted)
 {
     cin >> numberOfVertecies;
     graph = new vector<int>[numberOfVertecies];
-    cost = new vector<int>[numberOfVertecies];
+    weights = new vector<int>[numberOfVertecies];
     used = new bool[numberOfVertecies];
     prev = new int[numberOfVertecies];
 
@@ -59,17 +59,26 @@ void Graph::init()
 
     for (int i = 0; i < numberOfEdges; i++)
     {
-        int n, m;
+        int n, m, w;
         cin >> n >> m;
         graph[n].push_back(m);
         graph[m].push_back(n);
+
+        if (weighted)
+        {
+            cin >> w;
+            weights[n].push_back(w);
+            weights[m].push_back(w);
+        }
     }
 }
 
 void Graph::clear()
 {
     delete[] graph;
+    delete[] weights;
     delete[] used;
+    delete[] prev;
 }
 
 void Graph::clearUsed()
@@ -77,6 +86,10 @@ void Graph::clearUsed()
     for (int i = 0; i < numberOfVertecies; i++)
     {
         used[i] = false;
+    }
+    for (int i = 0; i < numberOfVertecies; i++)
+    {
+        prev[i] = -1;
     }
 }
 
@@ -116,14 +129,177 @@ void Graph::bfsPrint(int start)
     }
 }
 
+void Graph::findPathDfs(int curr, int end)
+{
+    used[curr] = true;
+    if (curr == end)
+    {
+        std::vector<int> path;
+        int i = curr;
+        while (i != -1)
+        {
+            path.push_back(i);
+            i = prev[i];
+        }
+
+        std::reverse(path.begin(), path.end());
+        for (auto i : path)
+        {
+            cout << i << " ";
+        }
+        cout << std::endl;
+
+        return;
+    }
+
+    for (int i = 0; i < graph[curr].size(); i++)
+    {
+        int sibling = graph[curr][i];
+        if (!used[sibling])
+        {
+            prev[sibling] = curr;
+            findPathDfs(sibling, end);
+        }
+    }
+}
+
+void Graph::findShortestPathBfs(int start, int end)
+{
+    queue<int> q;
+    q.push(start);
+    used[start] = true;
+
+    while (!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+
+        if (curr == end)
+        {
+            std::vector<int> path;
+            int i = curr;
+            while (i != -1)
+            {
+                path.push_back(i);
+                i = prev[i];
+            }
+            std::reverse(path.begin(), path.end());
+            for (auto i : path)
+            {
+                cout << i << " ";
+            }
+            cout << std::endl;
+
+            return;
+        }
+
+        for (int i = 0; i < graph[curr].size(); i++)
+        {
+            int sibling = graph[curr][i];
+            if (!used[sibling])
+            {
+                prev[sibling] = curr;
+                q.push(sibling);
+                used[sibling] = true;
+            }
+        }
+    }
+}
+
+void Graph::dijkstra(int start, int end)
+{
+    const int kinf_of_infinity = ~(1 << 31);
+    int *cost = new int[numberOfVertecies];
+    std::set<int> s;
+    used[start] = true;
+
+    for (int i = 0; i < numberOfVertecies; i++)
+    {
+        cost[i] = kinf_of_infinity;
+        s.insert(i);
+    }
+    cost[start] = 0;
+
+    while (!s.empty())
+    {
+        int curr = *s.begin();
+        for (auto i : s)
+        {
+            if (cost[i] < cost[curr])
+            {
+                curr = i;
+            }
+        }
+
+        s.erase(curr);
+        used[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++)
+        {
+            int sibling = graph[curr][i];
+            int weightFromStartToI = cost[curr] + weights[curr][i];
+            if (!used[sibling] && weightFromStartToI < cost[sibling])
+            {
+                cost[sibling] = weightFromStartToI;
+                prev[sibling] = curr;
+            }
+        }
+    }
+
+    cout << cost[end] << "\n";
+    std::vector<int> path;
+    int i = end;
+    while (i != -1)
+    {
+        path.push_back(i);
+        i = prev[i];
+    }
+
+    std::reverse(path.begin(), path.end());
+    for (auto i : path)
+    {
+        cout << i << " ";
+    }
+    cout << std::endl;
+
+    delete[] cost;
+}
+
 int main()
 {
-    Graph g();
+    Graph g(true);
     cout << "\n";
+    // g.findPathDfs(0, 5);
+    // g.clearUsed();
+    // g.findShortestPathBfs(0, 5);
+    // g.clearUsed();
+    g.dijkstra(5, 1);
 
     return 0;
 }
 
 /*
+7
+9
+0 1 1
+0 3 4
+0 4 1
+1 4 3
+2 3 5
+2 4 3
+2 5 3
+4 6 1
+5 6 10
 
+0 -> 1 2 3
+
+0 -> 4 6 10
+
+cost = [0, 1, i, 4, 1, i, i]
+used = [t, t, _, _, _, _, _]
+
+cost[1] = 1
+1 -> 4 = 3
+? 1 + 3 < cost[4] = 1;
+if yes -> cost[4] = 1 + 3
 */
